091be45cec62224f183aca116cc3d91a63fc9fe5
liulongbiao <liulongbiao@gmail.com>
2020-01-10 17:53:41 +0800

    refactor: 回退 Android 插件 API 版本

--- android/app/src/main/kotlin/com/kpg/agora_class_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/kpg/agora_class_demo/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.kpg.agora_class_demo
 
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
     }
 }
